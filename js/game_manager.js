f98cf916e2e20c9e3323bcff3689675a7fa23b6a
Jeong YunWon <jeong@youknowone.org>
2014-03-14 02:56:54 +0900

    4096

--- js/game_manager.js
+++ js/game_manager.js
@@ -122,8 +122,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 4096 tile
+          if (merged.value === 4096) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
